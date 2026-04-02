f059497612757f6b15debce5f68c5157061414c5
Mho7 <mholove7@gmail.com>
2026-04-02 13:28:41 +0900

    카메라 테스트

---
+++ cameratest.py
@@ -0,0 +1,11 @@
+import cv2
+
+for i in range(5):
+    cap = cv2.VideoCapture(i)
+    ret, frame = cap.read()
+    if ret:
+        print(f"{i} <- 현재 이 카메라가 연동된 상태임")
+        cv2.imshow(f"카메라 {i}", frame)
+        cv2.waitKey(0)
+        cap.release()
+        break
