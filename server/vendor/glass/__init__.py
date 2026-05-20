c7717c5cca5c382eb0ace288432cbc3fce333a8a
ParkHyunS1k <heoysik@naver.com>
2026-05-20 16:26:24 +0900

    글라스

--- server/vendor/glass/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import backbones
-from .glass import GLASS
-
-__all__ = ["backbones", "GLASS"]
---
+++ shell/run-spot.sh
@@ -0,0 +1,39 @@
+datapath=C:/Users/User/github/spot_catcher/data/glass_format
+augpath=C:/Users/User/Downloads/dtd-r1.0.1/dtd/images
+classes=('spot')
+flags=($(for class in "${classes[@]}"; do echo '-d '"${class}"; done))
+
+cd ..
+python main.py \
+    --gpu 0 \
+    --seed 0 \
+    --test ckpt \
+  net \
+    -b wideresnet50 \
+    -le layer2 \
+    -le layer3 \
+    --pretrain_embed_dimension 1536 \
+    --target_embed_dimension 1536 \
+    --patchsize 3 \
+    --meta_epochs 640 \
+    --eval_epochs 1 \
+    --dsc_layers 2 \
+    --dsc_hidden 1024 \
+    --pre_proj 1 \
+    --mining 1 \
+    --noise 0.015 \
+    --radius 0.75 \
+    --p 0.5 \
+    --step 20 \
+    --limit 392 \
+  dataset \
+    --distribution 2 \
+    --mean 0.5 \
+    --std 0.1 \
+    --fg 0 \
+    --rand_aug 1 \
+    --batch_size 4 \
+    --resize 288 \
+    --resize_w 512 \
+    --imagesize 288 \
+    --imagesize_w 512 "${flags[@]}" mvtec $datapath $augpath
